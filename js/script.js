feb89b4c6732f59517fc2a97e2f41a25d68a065a
Claudio Delkahez <cdelkahez.dev@gmail.com>
2022-02-21 17:28:55 -0300

    carousel finished

--- js/script.js
+++ js/script.js
@@ -1,16 +1,30 @@
 const slider = document.querySelector('.slider');
 const leftArrow = document.querySelector('.left');
 const rightArrow = document.querySelector('.right');
+const indicatorParents = document.querySelector('.controls ul')
+
+function setIndex(index) {
+    document.querySelector('.controls .selected').classList.remove('selected')
+    slider.style.transform = 'translate(' + (index) * -25 + '%)'
+}
 
 let sectionIndex = 0;
 
+document.querySelectorAll('.controls li').forEach((indicator, ind) => {
+    indicator.addEventListener('click', function () {
+        sectionIndex = ind;
+        indicator.classList.add('selected')
+        setIndex(sectionIndex)
+    })
+})
+
 rightArrow.addEventListener('click', () => {
     sectionIndex = (sectionIndex < 3) ? sectionIndex + 1 : 3
-    slider.style.transform = 'translate(' + (sectionIndex) * -25 + '%)'
-
+    indicatorParents.children[sectionIndex].classList.add('selected')
+    setIndex(sectionIndex)
 })
 leftArrow.addEventListener('click', () => {
     sectionIndex = (sectionIndex > 0) ? sectionIndex - 1 : 0
-    slider.style.transform = 'translate(' + (sectionIndex) * -25 + '%)'
-
+    indicatorParents.children[sectionIndex].classList.add('selected')
+    setIndex(sectionIndex)
 })
